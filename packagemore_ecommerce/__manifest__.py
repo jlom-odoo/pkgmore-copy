a3bb55fcfa3219692643e5de728fd31c98f78206
jden-odoo <jden@odoo.com>
2023-03-03 10:12:36 -0500

    [ADD] implemented ticket

---
+++ packagemore_ecommerce/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ecommerce',
+
+    'summary': 'Eommerce',
+
+    'description': """
+    Task ID: 3108813
+    Written by: jden
+    """,
+
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com/',
+
+    'category': 'Custom Development',
+    'version': '1.1',
+    'license': 'OPL-1',
+    'depends': ['website_sale', 'contacts', 'sale_management'],
+    'data': [
+        'views/product_template_views.xml'
+    ],
+}
---
+++ packagemore_ecommerce/models/product_template.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, _
+
+
+class ProductTemplate(models.Model):
+    _inherit='product.template'
+
+    exclusive_customer = fields.Many2one(
+        comodel_name='res.partner',
+        string="Exclusive customer",
+        help=_("""
+        When this field is not set, products are visible to all partners on ecommerce. 
+        When a partner is set, product is only visible to that partner on ecommerce
+        """)
+    )
+    
