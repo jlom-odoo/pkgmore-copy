9e41c563aa57aecdd238ccb8520776cc6fdbd6be
jden-odoo <jden@odoo.com>
2023-03-06 12:42:28 -0500

    added back test tag

--- packagemore_ecommerce/tests/ecommerce_tests.py
+++ packagemore_ecommerce/tests/ecommerce_tests.py
@@ -3,7 +3,7 @@ from odoo.tests import tagged, common
 # from odoo.tests.common import TransactionCase
 # from odoo.addons.website_sale.tests.test_sitemap import TestSitemap
 
-@tagged('post_install', '-at_install')
+@tagged('post_install', '-at_install', 'package')
 class NewTestSitemap(common.TransactionCase):
 
     def setUp(self):
