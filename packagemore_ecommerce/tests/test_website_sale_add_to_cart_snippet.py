673cba7ef2cf4a507ae2b6f7ab329012181c2a0c
jden-odoo <jden@odoo.com>
2023-03-10 12:43:55 -0500

    [FIX] unit test patch fix

--- packagemore_ecommerce/tests/test_website_sale_add_to_cart_snippet.py
+++ packagemore_ecommerce/tests/test_website_sale_add_to_cart_snippet.py
@@ -1,8 +1,8 @@
 from odoo.addons.website_sale.tests.test_website_sale_add_to_cart_snippet import TestAddToCartSnippet
 
 
+@tagged('post_install', '-at_install')
 class OverrideTestAddToCartSnippet(TestAddToCartSnippet):
-
     def test_configure_product(self):
         #This test will fail at checkout because of the behavior of this dev. 
         pass
