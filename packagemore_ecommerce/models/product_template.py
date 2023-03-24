474a110ab88780378cc65a8a5f86dd3c265d52ed
jden-odoo <jden@odoo.com>
2023-03-24 10:58:17 -0400

    [IMP] changed to exclusive company/partner

--- packagemore_ecommerce/models/product_template.py
+++ packagemore_ecommerce/models/product_template.py
@@ -4,9 +4,10 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit='product.template'
 
-    exclusive_customer = fields.Many2one(
-        comodel_name='res.users',
-        string='Exclusive customer',
-        help='When this field is not set, products are visible to all partners on ecommerce. When a partner is set, product is only visible to that partner on ecommerce',
+    exclusive_partner = fields.Many2one(
+        comodel_name='res.partner',
+        string='Exclusive partner',
+        help="""When this field is not set, products are visible to all partners on ecommerce. 
+        When a company is set, product is only visible to that partners within that company""",
     )
     
