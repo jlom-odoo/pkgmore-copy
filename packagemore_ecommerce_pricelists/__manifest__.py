877ceb4cf9fb274b95bbbf162ebe9df6029cf113
elce-odoo <elce@odoo.com>
2023-07-21 15:16:49 -0600

    [ADD] start ecommerce_pricelist

---
+++ packagemore_ecommerce_pricelists/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Package More Ecommerce Pricelists",
+    "summary": """Package More Ecommerce Pricelists: Show minimum quantity from pricelist""",
+    "description": """
+        If there is a minimum quantity defined on the pricelist, show the minimum quantity of the product for the selected pricelist on the website.
+
+        Developer: ELCE
+        Ticket ID: 3389883
+    """,
+    "author": "Odoo PS",
+    "website": "https://www.odoo.com",
+    "category": "Custom Development",
+    "version": "1.0",
+    "depends": [],
+    "data": [
+    ],
+    "license": "OPL-1",
+}
