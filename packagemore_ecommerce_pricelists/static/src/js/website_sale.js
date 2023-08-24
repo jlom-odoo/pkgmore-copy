b107eecf17b87b0368197400e372eebb1fc5ee26
elce-odoo <104387570+elce-odoo@users.noreply.github.com>
2023-08-24 16:20:09 -0600

    [TASK 3389883][IMP] being able to add min_qty to cart
    * Bypass Studio warnings
    
    * [ADD] start ecommerce_pricelist
    
    * [ADD] packagemore_ecommerce_pricelists
    
    * [IMP] only allow adding min_qty to cart
    
    * code review
    
    ---------
    
    Co-authored-by: EduardoCedillo(eacm) <eacm@odoo.com>
    Co-authored-by: Elisa Cervantes <elisacervantes@Elisas-MacBook-Air.local>

---
+++ packagemore_ecommerce_pricelists/static/src/js/website_sale.js
@@ -0,0 +1,25 @@
+odoo.define('packagemore_ecommerce_pricelists.website_sale_inherit', function (require) {
+    'use strict';
+
+    var websiteSaleWidget = require('website_sale.website_sale');
+
+    websiteSaleWidget.WebsiteSale.include({
+
+        /**
+         * @override
+         */
+
+        _onChangeCombination: function (ev, $parent, combination) {
+            this._super.apply(this, arguments);
+            var $qty = $('input[name="add_qty"]');
+            var $min_qty = $('input[name="qty-input"]');
+
+            const addToCart = $parent.find('#add_to_cart_wrap_2');
+            if ($qty.val() < $min_qty.val()) {
+                addToCart.removeClass('d-inline-flex').addClass('d-none');
+            } else {
+                addToCart.removeClass('d-none').addClass('d-inline-flex');
+            }
+        },
+    });
+});
