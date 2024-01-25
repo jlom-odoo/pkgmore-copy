4e3d46f33d7771be7616ebba9f6a406febf4bd37
wiwu-odoo <wiwu@odoo.com>
2024-01-25 16:07:10 -0500

    [FIX] code review changes

--- packagemore_ecommerce/__manifest__.py
+++ packagemore_ecommerce/__manifest__.py
@@ -12,8 +12,9 @@
     be able to view that product in the shop.
     """,
 
-    'author': 'Odoo, Inc',
-    'website': 'https://www.odoo.com/',
+    'author': 'Odoo Development Services',
+    'maintainer': 'Odoo Development Services',
+    'website': 'https://www.odoo.com',
 
     'category': 'Custom Development',
     'version': '1.0.2',
