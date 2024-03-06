7f5eac7ccabb146c273d680eeac77c95bd59b1d1
Brian <brc@odoo.com>
2024-03-05 18:13:32 -0800

    [ADD] 3pa update

--- pricelist_portal_download/static/src/js/portal.js
+++ pricelist_portal_download/static/src/js/portal.js
@@ -1,11 +1,6 @@
 /** @odoo-module */
-
 import publicWidget from 'web.public.widget';
-
 publicWidget.registry.PortalHomeCounters.include({
-    /**
-     * @override
-     */
     _getCountersAlwaysDisplayed() {
         return this._super(...arguments).concat(['pricelist_count']);
     },
