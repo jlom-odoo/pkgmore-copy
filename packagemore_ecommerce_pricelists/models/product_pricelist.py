d2e0b99ff9a0aa7fc412edcd0883ad2e5b783502
JLOM <jlom@odoo.com>
2024-03-14 08:51:23 -0600

    [FIX] packagemore_ecommerce_pricelists: Get min qty in all prlst itms
    After latest IMP on website product list view, unexpected behavior
    appeared: when having 2 or more pricelist items, setting the qty
    above the min_quantity of a higher pricelist item did not update
    the price to pay.
    
    This is a fix to that unexpected behavior while keeping the feature
    in product list view of showing the product price of the smallest
    min_quantity among its pricelist items even when this smallest
    min_quantity is greater than 1.
    
    opw-3389883

--- packagemore_ecommerce_pricelists/models/product_pricelist.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-
-class Pricelist(models.Model):
-    _inherit = "product.pricelist"
-
-    def _get_applicable_rules(self, products, date, **kwargs):
-        applicable_rules = super()._get_applicable_rules(products, date, **kwargs)
-        return applicable_rules.sorted("min_quantity")
